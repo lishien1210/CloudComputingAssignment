cef35faf52596d87fb472078e25d4cd7c0a31d7c
lishien1210 <57692804+lishien1210@users.noreply.github.com>
2021-11-23 20:56:32 +0800

    Create mapreduce.py

---
+++ mapreduce.py
@@ -0,0 +1,17 @@
+
+        
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+class WordCount(MRJob):
+def steps(self):
+return [
+  MRStep(mapper=self.mapper_get_word,
+  reducer=self.reducer_count_word)
+]
+def mapper_get_word(self, _, line):
+( ) = line.split('\t')
+yield word, 1
+def reducer_count_ratings(self, key, values):
+yield key, sum(values)
+if __name__ == '__main__':
+RatingsBreakdown.run()
